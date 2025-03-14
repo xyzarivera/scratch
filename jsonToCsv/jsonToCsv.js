fe54bd74780e1cf734bc04cf175af6985b98b40d
Xyza Rivera <xyzarivera@gmail.com>
2025-03-14 16:22:23 +0900

    add JSdoc for jsonToCsv

--- jsonToCsv/jsonToCsv.js
+++ jsonToCsv/jsonToCsv.js
@@ -1,12 +1,16 @@
 import { writeFileSync } from 'fs';
 
+/**
+ * This function converts an array of objects into a CSV format.
+ * It creates a CSV file (without the headers at the moment).
+ * It also creates a text file with the CSV in a single-line string
+ * format that can be used as an object value
+ */
 const jsonToCsv = (json) => {
   const csvLines = []
 
   json.forEach((object) => {
-    const line = Object.values(object).join(',')
-    console.log({line})
-   csvLines.push(line)
+    csvLines.push(Object.values(object).join(','))
   })
 
   const csv = csvLines.join('\n')
@@ -15,9 +19,8 @@ const jsonToCsv = (json) => {
   const csvString = csvLines.join('\\n')
   writeFileSync('test.txt', csvString, 'utf8')
 
-  return {csv}
+  return csv
 }
 
-const result = jsonToCsv([])
-console.log( result )
+jsonToCsv([])
 
